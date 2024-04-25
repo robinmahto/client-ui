0264c26e796baf9e499df2d1c3dbda3f707cd3b5
Robin Mahto <58412832+robinmahto@users.noreply.github.com>
2024-04-25 21:42:39 +0000

    add shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       <h1>welocme to our applications</h1>
+      <Button>Add</Button>
     </main>
   );
 }
